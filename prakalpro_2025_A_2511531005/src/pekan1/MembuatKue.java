913f5d1429b981515d42d6f6dd76e4a7d3c686fc
ASUS <ASUS@DESKTOP-IO1L0CU>
2025-09-08 14:59:19 +0700

    praktikum 1 kelas A

---
+++ prakalpro_2025_A_2511531005/src/pekan1/MembuatKue.java
@@ -0,0 +1,35 @@
+package pekan1;
+
+public class MembuatKue {
+
+	public static void main(String[] args) {
+		makeBatter();
+		memanggang();
+		memanggang();
+		menghias();
+	}
+	// Langkah 1 : Buat adonan kue.
+	public static void makeBatter () {
+		System.out.println("campur bahan kering.");
+		System.out.println("krim mentega dan gula.");
+		System.out.println("kocok telurnya.");
+	    System.out.println("masukkan bahan kering.");
+	   
+	}
+	// Langkah 2 : pangggang sekumpulan kue.
+	public static void memanggang() {
+		System.out.println("stel suhu oven.");
+		System.out.println("stel pengatur waktu.");
+		System.out.println("maukkan kue dalam oven.");
+		System.out.println("biarkan cookie untuk dipanggang.");
+	
+	}
+	// Langkah 3 : hiasi kue.
+	public static void menghias() {
+		System.out.println("campur bahan untuk froasting.");
+		System.out.println("taburkan froasting dan taburan.");
+		
+	}
+		
+	}
+
---
+++ prakalpro_2025_A_2511531005/src/pekan1/helloworld.java
@@ -0,0 +1,19 @@
+package pekan1;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println("Program ini menghasilkan");
+		System.out.println("empat baris output");
+		System.out.println("/ \\ // \\\\ /// \\\\");
+		System.out.println("this"+"program print a\n "
+					+"qoute from the gettyburg address.");
+		
+
+
+	}
+
+}
